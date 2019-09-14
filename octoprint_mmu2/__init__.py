29c61234ff73fa88214f488e84dc3558349e9523
chriswal <chris_______@hotmail.com>
2019-09-14 23:02:54 +0200

    printer handling

--- octoprint_mmu2/__init__.py
+++ octoprint_mmu2/__init__.py
@@ -13,12 +13,6 @@ import urllib2
 import threading
 import time
 
-mmu2_ser = serial.Serial(port=None)
-next_filament = ""
-old_filament = ""
-toolchange_detected = False
-
-
 
 class MMU2Plugin(octoprint.plugin.StartupPlugin,
 				octoprint.plugin.SettingsPlugin,
@@ -26,27 +20,43 @@ class MMU2Plugin(octoprint.plugin.StartupPlugin,
 				octoprint.plugin.TemplatePlugin,
 				octoprint.plugin.ShutdownPlugin):
 
+	def __init__(self):
+		self.mmu2_ser = serial.Serial(port=None)
+		self.next_filament = ""
+		self.old_filament = ""
+		self.toolchange_detected = False
+		self.timeout = 0
+		self.erhtime = 0
 
 	def on_after_startup(self):
 		self._logger.info("mmu2 plugin started")
 		self._serialport=self._settings.get(["serialport"])
 		self._baudrate=self._settings.get(["baudrate"])
+		self._timeout=self._settings.get(["timeout"])
+		self._erhtime=self._settings.get(["erhtime"])
 		self._stoppbits=self._settings.get(["stoppbits"])
 		self._bytesize=self._settings.get(["bytesize"])
 		self._parity=self._settings.get(["parity"])
-		self.reset_MMU2(self._serialport,self._baudrate)
+		self._grabfilament=self._settings.get(["grabfilament"])
+		self._feedtonozzle=self._settings.get(["feedtonozzle"])
+		self.reset_MMU2(self._serialport, self._baudrate)
+		global timeout
+		timeout = float(self._timeout)
+		global erhtime
+		erhtime = float(self._erhtime)
 		try:
+			global mmu2_ser
 			mmu2_ser = serial.Serial(
 				port=self._serialport,
 				baudrate=115200,
-				timeout=0.5,
+				timeout=float(self._timeout),
 				write_timeout=0.5
 				#parity=serial.PARITY_NONE,
 				#stopbits=serial.STOPBITS_ONE,
 				#bytesize=serial.EIGHTBITS
 				)
 		except ValueError:
-			self._logger.error("serial port definition error")
+			self._logger.error("serial port definition error %s %s %s" % (self._serialport, self._baudrate, self._timeout))
 		except serial.SerialException:
 			self._logger.error("cannot open com port for mmu2")
 		else:
@@ -80,7 +90,10 @@ class MMU2Plugin(octoprint.plugin.StartupPlugin,
 					stoppbits="0",
 					bytesize="8",
 					parity="None",
-					timeout=30,
+					timeout=60.1,
+					erhtime=30,
+					grabFilament="",
+					feedtonozzle="",
 					mmu2commands=dict(
 						okresponse="ok",
 						checkpresent="S0",
@@ -150,15 +163,16 @@ class MMU2Plugin(octoprint.plugin.StartupPlugin,
 		if (gcode and cmd == "T0") or (gcode and cmd == "T1") or (gcode and cmd == "T2") or (gcode and cmd == "T3") or (gcode and cmd == "T4"):
 			global old_filament
 			global next_filament
-			old_filament = next_filament
-			next_filament = cmd[-1:]
-			self._logger.info("toolchange detected %s" % next_filament)
-			self._printer.set_job_on_hold(True)
+			self.old_filament = self.next_filament
+			self.next_filament = cmd[-1:]
 			cmd = None
-			global toolchange_detected
-			toolchange_detected = True
-			handle_tool_change = threading.Thread(target=self.handle_filament_change, args=())
-			handle_tool_change.start()
+			if (self.old_filament != self.next_filament):
+				self._logger.info("toolchange detected %s" % self.next_filament)
+				self._printer.set_job_on_hold(True)
+				global toolchange_detected
+				toolchange_detected = True
+				handle_tool_change = threading.Thread(target=self.handle_filament_change, args=())
+				handle_tool_change.start()
 		return cmd,
 
 	def sent_pause(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
@@ -166,16 +180,16 @@ class MMU2Plugin(octoprint.plugin.StartupPlugin,
 		if gcode and cmd == "pause":
 			self._logger.info("Just sent T: {cmd}".format(**locals()))
 
-	def reset_MMU2(self,serialport,baudrate):
-		port = self.open_serial_port(serialport,baudrate)
+	def reset_MMU2(self,serialport, baudrate, timeout=0):
+		port = self.open_serial_port(serialport, baudrate, timeout)
 		if port:
-			self.send_MMU2_command(port,"X0")
+			self.send_MMU2_command(port, "X0")
 			time.sleep(1)
 			port.close()
 
-	def send_MMU2_command(self,port,command):
+	def send_MMU2_command(self, port, command):
 		try:
-			self._logger.info("Port used %s %d" % (port.name,port.baudrate))
+			self._logger.info("Port used %s %d" % (port.name, port.baudrate))
 			port.write(command+"\n")
 		except serial.portNotOpenError:
 			self._logger.error("Port not open to MMU2")
@@ -184,32 +198,69 @@ class MMU2Plugin(octoprint.plugin.StartupPlugin,
 		else:
 			self._logger.info("Command %s written to MMU2" % command)
 
-	def open_serial_port(self,serialport,baudrate):
+	def open_serial_port(self, serialport, baudrate, timeout):
 		try:
 			mmu2_ser = serial.Serial(
 				port=serialport,
 				baudrate=baudrate,
-				timeout=0.5,
+				timeout=timeout,
 				write_timeout=0.5
 				#parity=serial.PARITY_NONE,
 				#stopbits=serial.STOPBITS_ONE,
 				#bytesize=serial.EIGHTBITS
 				)
 		except ValueError:
-			self._logger.error("serial port definition error")
+			self._logger.error("serial port definition error %s %d %d" % serialport, baudrate, timeout)
 		except serial.SerialException:
 			self._logger.error("cannot open com port for mmu2")
 		else:
 			self._logger.info("serial port for mmu2 open")
 			return mmu2_ser
 
-	def send_printer_command(self,cmd, tags):
+	def send_printer_command(self, cmd, tags):
 		self._printer.commands(cmd, None)
+
+	def flush_ser_buffer(self, port, timeout):
+		port.reset_input_buffer()
+		port.reset_output_buffer()
+
+	def wait_for_ok(self, port, timeout):
+		mmu2_ok = False
+		answer = port.read(size=3)[0:-1]
+		if answer == "ok":
+			mmu2_ok = True
+		self._logger.info("Answer %s" % answer)
+		return mmu2_ok
 
 	def handle_filament_change(self):
 		self._logger.info("Filament change")
-		self._printer.set_job_on_hold(False)
-		#		self.send_printer_command("@resume", "None")
+		global toolchange_detected
+		global next_filament
+		global mmu2_ser
+		if toolchange_detected:
+			global mmu2_ser
+			self.flush_ser_buffer(mmu2_ser, 0)
+			self.send_printer_command(("G91", "G1 E-30 F300", "G90"), None)
+			self.send_MMU2_command(mmu2_ser, ("T"+self.next_filament).encode("UTF8"))
+			ok = False
+			global timeout
+			global erhtime
+			i = (erhtime*60)/timeout
+			while not ok or i < 0:
+				i -= 1
+				ok = self.wait_for_ok(mmu2_ser, 20)
+			#else:
+				#self._printer.set_job_on_hold(False)
+			self.send_MMU2_command(mmu2_ser, "C0".encode("UTF8"))
+			ok = False
+			self.send_printer_command(("G91", "G1 E5 F300", "G90"), None)
+			time.sleep(1)
+			self.send_MMU2_command(mmu2_ser, "A".encode("UTF8"))
+			ok = self.wait_for_ok(mmu2_ser, 20)
+			self._printer.set_job_on_hold(False)
+
+
+#		self.send_printer_command("@resume", "None")
 
 
 def __plugin_load__():
